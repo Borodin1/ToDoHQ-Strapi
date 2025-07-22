e4a027b6b9c428945fc4ac0eedaa7a4cb6c9a829
Alexander Borodin <borodinag1902@gmail.com>
2025-07-22 12:24:14 +0300

    add cors

---
+++ src/config/middlewares.js
@@ -0,0 +1,36 @@
+module.exports = [
+  "strapi::errors",
+
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "https:"],
+          "media-src": ["'self'", "data:", "blob:", "https:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://todohq-strapi.onrender.com"],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      headers: ["Content-Type", "Authorization"],
+      credentials: true,
+    },
+  },
+
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
